cb2db1788d7481269a47aa3a9ba89477c33587de
Veronika <Vasileva.s.veronika@gmail.com>
2023-11-30 20:11:56 +0300

    Добавила решение задачи 3

---
+++ tasks 003/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 3:
+
+Console.Write("Введите количество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+float [] arr = new float[size];
+Random r = new Random();
+
+for (int i = 0; i < size; i++)
+{
+    arr[i] = (float)r.NextDouble()*1000;
+    Console.WriteLine(arr[i]);
+}
+
+Console.WriteLine();
+
+float max = arr[0];
+float min = arr[0];
+for (int i = 0; i < size; i++)
+{
+    if (arr[i] > max)
+    max = arr[i];
+    if (arr[i] < min)
+    min = arr[i];
+}
+
+Console.WriteLine(max);
+Console.WriteLine(min);
+Console.Write($"\n Рфзница между максимальным и минимальным элементов значения массива: {max-min}");
+
