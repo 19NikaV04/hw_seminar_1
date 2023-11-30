5c7adfe49203c4b03003f69744610127ac5541b8
Veronika <Vasileva.s.veronika@gmail.com>
2023-11-30 19:41:24 +0300

    Добавила решение задачи 1

---
+++ tasks 001/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задание 1:
+
+int size = 10;
+
+int [] a = new int[size];
+Random random = new Random();
+
+for (int i = 0; i < size; i++)
+{
+    a[i] = random.Next(1, 100);
+    Console.WriteLine(a[i]);
+}
+
+Console.WriteLine();
+
+int count = 0
+for (int i = 0; i < size; i++)
+{
+    if (a[i] >= 20 && a[i] <= 90)
+    count++;
+}
+Console.WriteLine();
+System.Console.WriteLine(count);
