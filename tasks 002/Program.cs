a3ed064ee0daabdcd867e3c429879e97d7039257
Veronika <Vasileva.s.veronika@gmail.com>
2023-11-30 19:56:04 +0300

    Добавила решение задачи 2

---
+++ tasks 002/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задание 2:
+
+int size = 10;
+
+int [] a = new int[size];
+Random random = new Random();
+
+int count = 0
+for (int i = 0; i < size; i++)
+{
+    a[i] = random.Next(1, 100);
+    Console.WriteLine(a[i]);
+
+    if (a[i] % 2 == 0)
+    count++;
+}
+Console.WriteLine();
+System.Console.WriteLine(count);
+
